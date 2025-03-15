2e4f43b8c59d21a1cabdacc36bd01943d9e1c72e
alin <doublea654@gmail.com>
2025-03-15 17:28:52 +0100

    tank controls, no acceleration yet

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,7 +14,7 @@ var current_turn_speed = 0;
 @export var mech_turn_speed = 1;
 
 ##This is by how much the mech will start accelerating its turning. UNIT: Degrees/Second
-@export var mech_turn_acceleration = 1
+@export var mech_turn_acceleration = 1;
 
 ##This is the speed the mech will deccelerate and try to stop turning at. UNIT: Degrees/Second
 @export var mech_turn_decceleration = 1;
@@ -27,34 +27,25 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	#MOVEMENT HANDLING - UNFINISHED
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = 0 # The player's movement vector.
 	
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
 	if Input.is_action_pressed("move_backward"):
-		velocity.y += 1
+		velocity += 1
 	if Input.is_action_pressed("move_forward"):
-		velocity.y -= 1
-	if velocity.length() > 0: #make sure we dont move faster diagonally
-		velocity = velocity.normalized() * move_speed
-	position += velocity * delta # velocity just helps us move, this is actually what sets our position
+		velocity -= 1
+	#if velocity.length() > 0: #make sure we dont move faster diagonally
+	#	velocity = velocity.normalized() * move_speed
+		
 	
 	
 	#TURN HANDLING
 	var turn_direction: int = 0 # LEFT IS -1, RIGHT IS 1
 	
-	
 	if Input.is_action_pressed("turn_left"):
 		turn_direction += -1;
-		
-
 	if Input.is_action_pressed("turn_right"):
 		turn_direction += 1;
 		
-	
-	
 	
 	if (abs(current_turn_speed) <= mech_turn_decceleration): #makes the deccelleration stop the mech once it gets close enough to 0
 		current_turn_speed = 0
@@ -68,5 +59,9 @@ func _process(delta: float) -> void:
 	current_turn_speed = clamp(current_turn_speed, -mech_max_turn_speed, mech_max_turn_speed)
 	
 	current_turn_speed += (turn_direction * mech_turn_speed)
-
+	
 	rotation_degrees += (current_turn_speed) * delta;
+	
+	#FUNKY MATH, DON'T TOUCH THIS
+	position.y += ((velocity*move_speed)*cos(rotation)) * delta
+	position.x += ((velocity*move_speed)*-sin(rotation)) * delta

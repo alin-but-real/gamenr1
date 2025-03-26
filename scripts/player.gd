06ea239d6013f1a675c51f727c82eda9ec58137b
alin <doublea654@gmail.com>
2025-03-26 11:53:10 +0100

    prettied up movement code so its readable

--- scripts/player.gd
+++ scripts/player.gd
@@ -25,19 +25,7 @@ func _ready() -> void:
 	print_debug("player awake")
 
 func _process(delta: float) -> void:
-	
-	#MOVEMENT HANDLING - UNFINISHED
-	var velocity = 0 # The player's movement vector.
-	
-	if Input.is_action_pressed("move_backward"):
-		velocity += 1
-	if Input.is_action_pressed("move_forward"):
-		velocity -= 1
-	#if velocity.length() > 0: #make sure we dont move faster diagonally
-	#	velocity = velocity.normalized() * move_speed
-		
-	
-	
+
 	#TURN HANDLING
 	var turn_direction: int = 0 # LEFT IS -1, RIGHT IS 1
 	
@@ -46,7 +34,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("turn_right"):
 		turn_direction += 1;
 		
-	
 	if (abs(current_turn_speed) <= mech_turn_decceleration): #makes the deccelleration stop the mech once it gets close enough to 0
 		current_turn_speed = 0
 	elif (current_turn_speed < 0): 
@@ -61,7 +48,13 @@ func _process(delta: float) -> void:
 	current_turn_speed += (turn_direction * mech_turn_speed)
 	
 	rotation_degrees += (current_turn_speed) * delta;
+	#MOVEMENT HANDLING - UNFINISHED
+	var velocity = 0 # The player's movement vector.
 	
-	#FUNKY MATH, DON'T TOUCH THIS
+	if Input.is_action_pressed("move_backward"):
+		velocity += 1
+	if Input.is_action_pressed("move_forward"):
+		velocity -= 1
+	
 	position.y += ((velocity*move_speed)*cos(rotation)) * delta
 	position.x += ((velocity*move_speed)*-sin(rotation)) * delta

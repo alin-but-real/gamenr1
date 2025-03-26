5bff92a9316eb5543efc3d422b6c99739a7a62b1
alin <doublea654@gmail.com>
2025-03-26 12:40:22 +0100

    dash code 1, not done but skeleton done

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,10 +9,22 @@ extends Area2D
 ##This is how much the mech will deccelerate by each frame, when not holding down any movement key.
 @export var move_decceleration = 1
 
+##This is the cooldown before the player is allowed to dash again, measured in milliseconds. (1s = 1000ms)
+@export var dash_cooldown = 2000
+##This is the speed the player will gain when dashing. It's also related to the time it takes to dash and is jank af.
+@export var dash_speed = 1000;
+##This is the decceleration the player will experience each frame after a dash. So if speed = 1000, decel = 200, it's 5 frames of dash.
+@export var dash_decceleration = 200
 
 ##This is the current turn speed of the player. It glides towards 0, and caps at max_turn_speed
 var current_turn_speed = 0;
+##This is the current move speed of the player. It glides towards 0, and caps at max_move_speed
 var current_move_speed = 0;
+##This is the current dash cooldown of the player. It should be 0 most of the time, and sets to dash_cooldown when dashing.
+var current_dash_cooldown = 0;
+##This is the current dash speed of the player. It should be 0 most of the time.
+var current_dash_speed = 0;
+
 
 
 
@@ -81,3 +93,35 @@ func _process(delta: float) -> void:
 	
 	position.y += current_move_speed*-cos(rotation) * delta
 	position.x += current_move_speed*sin(rotation) * delta
+	
+	#DASH HANDLING - Q AND E
+	var dash_direction = 0 #left is -1, right is +1
+	
+	
+	if Input.is_action_pressed("move_left"): #THESE DONT EXIST YET
+		dash_direction -= 1
+	if Input.is_action_pressed("move_right"):
+		dash_direction += 1
+	
+	
+	if (abs(current_dash_speed) <= dash_decceleration): #makes the deccelleration stop the mech once it gets close enough to 0
+		current_dash_speed = 0
+	elif (current_dash_speed < 0): 
+		current_dash_speed += dash_decceleration
+	else:
+		current_dash_speed -= dash_decceleration
+	
+	
+	
+	if (current_dash_cooldown) == 0:
+		current_dash_cooldown = dash_cooldown
+		current_dash_speed = dash_direction * dash_speed
+	else:
+		current_dash_cooldown -= 20
+		position.y += current_dash_speed*-cos(rotation + 0.5*dash_direction) * delta
+		position.x += current_dash_speed*sin(rotation + 0.5*dash_direction) * delta
+
+	
+	
+	
+	

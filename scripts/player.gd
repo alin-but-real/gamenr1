26ae9f4ce21461994fbe2d7f1b2c10199e55db55
alin <doublea654@gmail.com>
2025-03-26 12:15:59 +0100

    move speed acceleration done

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,18 @@
 extends Area2D
+
+##This is the maximum move speed of the player.
+@export var move_speed_max = 2000
+##This is the speed the mech will move at base, without any acceleration.
 @export var move_speed = 1000
+##This is how much the mech will accelerate by each frame.
 @export var move_accel = 1
+##This is how much the mech will deccelerate by each frame, when not holding down any movement key.
+@export var move_decceleration = 1
+
 
 ##This is the current turn speed of the player. It glides towards 0, and caps at max_turn_speed
 var current_turn_speed = 0;
+var current_move_speed = 0;
 
 
 
@@ -46,16 +55,29 @@ func _process(delta: float) -> void:
 	#second, clamp the value so it doesn't turn too fast
 	current_turn_speed = clamp(current_turn_speed, -mech_max_turn_speed, mech_max_turn_speed)
 	
-	#current_turn_speed += (turn_direction * mech_turn_speed)
-	
 	rotation_degrees += (current_turn_speed) * delta;
+	
+	
 	#MOVEMENT HANDLING - UNFINISHED
-	var velocity = 0 # The player's movement vector.
+	var direction = 0 # The player's movement vector. describes wether he's moving forward or not +1 forward, -1 backward
 	
-	if Input.is_action_pressed("move_backward"):
-		velocity += 1
 	if Input.is_action_pressed("move_forward"):
-		velocity -= 1
+		direction += 1
+	if Input.is_action_pressed("move_backward"):
+		direction -= 1
 	
-	position.y += ((velocity*move_speed)*cos(rotation)) * delta
-	position.x += ((velocity*move_speed)*-sin(rotation)) * delta
+	if (abs(current_move_speed) <= move_decceleration): #makes the deccelleration stop the mech once it gets close enough to 0
+		current_move_speed = 0
+	elif (current_move_speed < 0): 
+		current_move_speed += move_decceleration
+	else:
+		current_move_speed -= move_decceleration
+	
+	current_move_speed += direction * (move_speed + move_decceleration)
+	
+	current_move_speed = clamp(current_move_speed, -move_speed_max, move_speed_max)
+	
+	print_debug(current_move_speed)
+	
+	position.y += current_move_speed*-cos(rotation) * delta
+	position.x += current_move_speed*sin(rotation) * delta

06343b7e5a8005d80359d218e8a26d467a1db6a9
alin <doublea654@gmail.com>
2025-03-29 18:07:18 +0100

    fixed bullets, needs prettying up still

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -1,7 +1,15 @@
 extends Area2D
 
+@export var bullet_speed = 1000
+@export var bullet_lifetime = 2
+
 func _ready() -> void:
 	print_debug("bullet fired")
+	$Timer.wait_time = bullet_lifetime
+
+func _process(delta: float) -> void:
+	position.y += bullet_speed*-cos(rotation) * delta
+	position.x += bullet_speed*sin(rotation) * delta
 
 func _on_timer_timeout() -> void:
 	queue_free()
--- scripts/weapon_projectile.gd
+++ scripts/weapon_projectile.gd
@@ -1,14 +1,19 @@
 extends Node2D
 @export var projectile: PackedScene
 
+var player
+var weapon_slot
 
 func _ready():
-	var parent = get_parent()
-	parent.fire_weapon.connect(_on_fire_weapon)
+	#this section ties the code together for firing the weapons to the weapon's slot, and position data
+	weapon_slot = get_parent()
+	weapon_slot.fire_weapon.connect(_on_fire_weapon)
 
 func _on_fire_weapon():
 	var bullet_instance = projectile.instantiate()
-	get_node("Main").add_child(bullet_instance) #THIS IS BUGGED. GET_NODE NEEDS TO REFER TO THE WORLD, THEN THIS WORKS
+	bullet_instance.rotation = weapon_slot.global_rotation
+	bullet_instance.position = weapon_slot.global_position
+	get_node("/root/Main").add_child(bullet_instance) #THIS IS REALLY FRAGILE IF WE MOVE MAIN.
 	
 	
 	
